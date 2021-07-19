0cfa857e79615735e74d2c2300259caca1ab18ab
andreaax <75586588+andreaax@users.noreply.github.com>
2021-07-18 23:18:33 -0400

    added project

---
+++ src/Library.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class FinalProject {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Hurtado Library!");
+        System.out.println("Please begin by entering your first name:");
+        String fName = scanner.nextLine();
+    }
+}
